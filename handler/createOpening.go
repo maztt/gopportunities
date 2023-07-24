6c2a70dbad2429037f388e14aa55cbc8f0b5a8f9
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-07-24 15:25:30 -0300

    feat: add createOpening validation handler

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -4,10 +4,33 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/maztt/gopportunities/schemas"
 )
 
 func CreateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "POST.opening",
-	})
+	request := CreateOpeningRequest{}
+
+	context.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		reportError(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		reportError(context, http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
+
+	reportSuccess(context, "create-opening", opening)
 }
