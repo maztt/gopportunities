d651c6470b061705ed0ceff34837bcf53e04bade
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-07-24 16:47:27 -0300

    feat: add deleteOpening validation handler

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,29 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/maztt/gopportunities/schemas"
 )
 
 func DeleteOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE.opening",
-	})
+	id := context.Query("id")
+	if id == "" {
+		reportError(context, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	// Find (and then) delete
+	if err := db.First(&opening, id).Error; err != nil {
+		reportError(context, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+	if err := db.Delete(&opening).Error; err != nil {
+		reportError(context, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id: %s", id))
+		return
+	}
+	reportSuccess(context, "delete-opening", opening)
 }
