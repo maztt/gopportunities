d218edd06614f5c43c75d51dec941974ca88f48b
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-07-24 16:49:06 -0300

    feat: add updateOpening validation handler

--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -4,10 +4,59 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/maztt/gopportunities/schemas"
 )
 
 func UpdateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "UPDATE.opening",
-	})
+	request := UpdateOpeningRequest{}
+
+	context.BindJSON(&request)
+
+	// Find (and then) update the opening
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		reportError(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := context.Query("id")
+	if id == "" {
+		reportError(context, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		reportError(context, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	// Save update
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("error updating opening: %v", err.Error())
+		reportError(context, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+
+	reportSuccess(context, "update-opening", opening)
 }
