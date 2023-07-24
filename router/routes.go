264b870263face6dfca0d758edcf5eb32ed2f091
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-07-24 16:50:14 -0300

    chore: add swagger configuration

--- router/routes.go
+++ router/routes.go
@@ -2,13 +2,19 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	docs "github.com/maztt/gopportunities/docs"
 	"github.com/maztt/gopportunities/handler"
+	swaggerfiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
 // "*" is a pointer; != "pass by value"; works like you are giving an address for the code to access
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		// gin.H works as a wrapper for the object
 		v1.GET("/opening", handler.ShowOpeningHandler)
@@ -17,4 +23,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	// Initialize Swagger
+	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
