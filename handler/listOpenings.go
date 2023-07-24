0a0c1ff66e81327c949efa17b468e904f74d3ae1
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-07-24 16:48:24 -0300

    feat: add listOpenings validation handler

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/maztt/gopportunities/schemas"
 )
 
 func ListOpeningsHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "GET.openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		reportError(context, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	reportSuccess(context, "list-openings", openings)
 }
